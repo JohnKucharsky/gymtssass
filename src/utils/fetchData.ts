0cee5943d34579fb0ea9e003c63f5bf19d961bf4
John <vannok@live.com>
2022-06-26 00:57:01 +0300

    cmmt

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "9a6d3f0137mshae6b39cf3d23b20p1c27e9jsne7385c2cc8b1",
+    "X-RapidAPI-Key": process.env.REACT_APP_KEY,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
@@ -10,7 +10,7 @@ export const youtubeOptions = {
   method: "GET",
 
   headers: {
-    "X-RapidAPI-Key": "9a6d3f0137mshae6b39cf3d23b20p1c27e9jsne7385c2cc8b1",
+    "X-RapidAPI-Key": process.env.REACT_APP_KEY,
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
